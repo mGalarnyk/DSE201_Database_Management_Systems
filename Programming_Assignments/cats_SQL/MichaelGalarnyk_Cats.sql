a1caa8003057ba9559313baa4062b3809e53bb93
mGalarnyk <mgalarny@gmail.com>
2016-02-06 21:39:17 -0800

    queries for sales and cats

---
+++ Programming_Assignments/cats_SQL/MichaelGalarnyk_Cats.sql
@@ -0,0 +1,108 @@
+﻿-- Query 1 “Overall Likes”: The Top-10 cat videos are the ones 
+-- that have collected the highest numbers of likes, overall.
+
+SELECT v.video_id, v.video_name, ordLikes.Likes_Overall 
+FROM 
+	(
+        SELECT video_id, COUNT(*) Likes_Overall
+        FROM cats.likes
+        GROUP BY video_id
+        ORDER BY Likes_Overall DESC LIMIT 10
+	) ordLikes
+JOIN cats.video v
+ON  v.video_id = ordLikes.video_id;
+
+-- Query 2 “Friend Likes”: The Top-10 cat videos are the ones that 
+-- have collected the highest numbers of likes from the friends of X.
+
+SELECT v.video_name, v.video_id, edith.CNT FriendLikes
+FROM 
+	(
+	SELECT L.video_id
+	  , COUNT(L.video_id) CNT
+	FROM cats.user u
+	    JOIN cats.friend f
+	    ON  u.user_id = f.user_id
+	    JOIN cats.likes L
+	    ON  F.friend_id = L.user_id
+	WHERE u.user_name = 'Edith1Moore'
+	GROUP BY L.video_id
+	ORDER BY CNT DESC LIMIT 10
+	) edith
+JOIN cats.video v 
+ON edith.video_id = v.video_id;
+
+-- Query 3 “Friends-of-Friends Likes”: The Top-10 cat videos are 
+-- the ones that have collected the highest numbers of likes from 
+-- friends and friends-of-friends.
+
+SELECT v.video_id, v.video_name, E.CNT Distant_Likes
+FROM
+	(
+	SELECT L.video_id, COUNT(L.video_id) CNT
+	FROM CATS.USER U
+	    LEFT JOIN CATS.FRIEND F
+	    ON  U.user_id = F.user_id
+	    LEFT JOIN CATS.FRIEND FF
+	    ON  F.friend_ID = FF.user_id
+	    LEFT JOIN cats.likes L
+	    ON  FF.friend_id = L.user_id   
+		OR F.friend_ID = L.user_ID
+	WHERE USER_NAME = 'Edith1Moore'
+	    AND L.user_id != u.user_id
+	GROUP BY L.video_id
+	ORDER BY CNT DESC LIMIT 10
+	) E
+JOIN cats.video v
+ON  v.video_id = E.video_id;
+
+-- Query 4 “My kind of cats”: The Top-10 cat videos 
+-- are the ones that have collected the most likes from
+-- users who have liked at least one cat video that was liked by X.
+
+SELECT v.video_id, v.video_name, E.CNT Distant_Like
+FROM
+	(
+        SELECT p2.video_id
+          , COUNT(p2.like_id) CNT
+        FROM cats.user u
+            JOIN cats.likes p1
+            ON  u.user_id = p1.user_Id
+            JOIN cats.likes p2 
+            ON  p1.video_id = p2.video_id
+        WHERE u.user_name = 'Edith1Moore'
+        GROUP BY p2.video_id
+	) E
+JOIN cats.video v
+ON  v.video_id = E.video_id
+ORDER BY Distant_Like DESC LIMIT 10;
+
+-- Query 5  “My kind of cats – with preference 
+-- (to cat aficionados that have the same tastes)”
+
+WITH wusers AS
+    (
+    SELECT l2.user_id,
+    log(COUNT(l2.like_id) + 1) like_weigh
+    FROM cats.user u
+    JOIN cats.likes l 
+    ON  u.user_id = l.user_Id
+    JOIN cats.likes l2
+    ON  l2.video_id = l.video_id
+    WHERE u.user_name = 'Edith1Moore'
+    GROUP BY l2.user_id
+    ),
+wlikes AS
+    (
+    SELECT l.video_id,
+    SUM(w.like_weigh) sum_weigh
+    FROM cats.likes l
+    JOIN wusers w
+    ON l.user_id = w.user_id
+    GROUP BY l.video_id
+    ORDER BY sum_weigh DESC limit 10
+    )
+SELECT l.sum_weigh, v.video_name, v.video_id
+FROM wlikes l
+JOIN cats.video v
+ON  l.video_id = v.video_id;
---
+++ Programming_Assignments/sales_SQL/MichaelGalarnyk_Sales.sql
@@ -0,0 +1,83 @@
+﻿-- Query 1: Show the total sales (quantity sold and
+-- dollar value) for each customer
+
+SELECT c.customer_id, c.customer_name, sum(s.quantity) quantity_sold , sum(s.price) dollar_value
+FROM sales.sale s JOIN sales.customer c ON s.customer_id = c.customer_id
+GROUP BY c.customer_id, c.customer_name
+ORDER BY dollar_value DESC;
+
+
+-- Query 2: Show the total sales for each state.
+SELECT st.state_name, sum(s.quantity) quantity_sold, sum(s.price) dollar_value
+FROM sales.sale s 
+JOIN sales.customer c ON s.customer_id = c.customer_id
+JOIN sales.state st ON st.state_id = c.state_id
+GROUP BY st.state_name;
+
+-- Query 3: Show the total sales for each product, for a given customer. Only products that were actually
+-- bought by the given customer. Order by dollar value.
+
+SELECT cusProTot.customer_id, cusProTot.product_id, stotal, dollar_value, p.product_name, c.customer_name
+FROM     
+(
+	SELECT c.customer_id,p.product_id,SUM(s.quantity) sTotal,SUM(s.price) dollar_value
+	FROM sales.customer c
+	JOIN sales.sale s
+	ON  s.customer_id = c.customer_id
+	JOIN sales.product p
+	ON  s.product_id = p.product_id
+	WHERE c.customer_name = 'Stefanie Alexander'
+	GROUP BY c.customer_id, p.product_id
+) cusProTot
+JOIN sales.customer c
+ON  cusProTot.customer_id = c.customer_id
+JOIN sales.product p
+ON  p.product_id = cusProTot.product_id
+ORDER BY dollar_value DESC;
+
+-- Query 4: Show the total sales for each product and customer. Order by dollar value.
+
+SELECT c.customer_id, c.customer_name, p.product_name, sum(price) as dollar_value
+FROM sales.sale s JOIN sales.customer c ON s.customer_id = c.customer_id
+JOIN sales.product p ON p.product_id = s.product_id
+GROUP BY c.customer_id, c.customer_name, p.product_name
+ORDER by dollar_value DESC;
+
+-- Query 5: Show the total sales for each product category and state.
+SELECT cu.state_id, c.category_id, SUM(s.price) total_sales
+FROM sales.sale s, sales.category c, sales.product p, sales.customer cu
+WHERE s.product_id = p.product_id 	
+AND p.category_id = c.category_id 
+AND s.customer_id = cu.customer_id
+GROUP BY cu.state_id, c.category_id;
+
+
+-- Query 6: For each one of the top 20 product categories and top 20 customers, it returns a tuple (top
+-- product, top customer, quantity sold, dollar value)
+
+with top_20_categories as 
+(
+	select c.category_id, c.category_name, sum(s.quantity) quantity_sold, sum(s.price) dollar_value
+	from sales.sale as s
+	join sales.product as p on s.product_id = p.product_id
+	join sales.category as c on p.category_id = c.category_id
+	group by c.category_id, c.category_name
+	order by dollar_value DESC
+	limit 20
+),
+top_20_customers as 
+(
+	select CUS.customer_id, CUS.customer_name, sum(s.quantity) as quantity_sold, sum(s.price) dollar_value
+	from sales.sale as s
+	join sales.customer as CUS on s.customer_id=CUS.customer_id
+	group by CUS.customer_id, CUS.customer_name
+	order by dollar_value DESC
+	limit 20
+)
+select tcat.category_name as top_category, tc.customer_name as top_customer, sum(s.quantity) quantity_sold, sum(s.price) dollar_value
+from sales.sale s
+join sales.product as p on s.product_id=p.product_id
+join top_20_categories as tcat on p.category_id=tcat.category_id
+join top_20_customers as tc on s.customer_id=tc.customer_id
+group by tcat.category_name, tc.customer_name
+order by dollar_value DESC;
